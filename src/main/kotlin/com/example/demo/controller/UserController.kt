7af80a4de7f69727400788137d1048252e285b15
ian23722 <8547969+ian23722@users.noreply.github.com>
2022-07-03 11:18:10 +0900

    change return types from string to object.

--- src/main/kotlin/com/example/demo/controller/UserController.kt
+++ src/main/kotlin/com/example/demo/controller/UserController.kt
@@ -6,12 +6,12 @@ import com.example.demo.domain.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 
+@Suppress("unused")
 @Controller
 @RequestMapping(path=["/user"])
 class UserController {
@@ -24,42 +24,40 @@ class UserController {
     //TODO move to post mapping - login information cannot be a part of url because it creates security vulnerability.
     @PostMapping(path = ["/login"])
     @ResponseBody
-    fun login(@RequestParam("username") username: String, @RequestParam("password") password: String): String {
-        val user: User = repository.findByUsername(username) ?: return """
-            {"login":"failed"}
-        """.trimIndent()
+    fun login(@RequestParam("username") username: String, @RequestParam("password") password: String): LoginResponse {
+        val user: User = repository.findByUsername(username) ?: return LoginResponse("failed")
         return if (passwordEncoder.matches(password, user.password)) {
-            """
-            {"login":"succeeded"}
-            """.trimIndent()
+            LoginResponse("succeeded")
         } else {
-            """
-            {"login":"failed"}
-            """.trimIndent()
+            LoginResponse("failed")
         }
     }
+
+    data class LoginResponse(val login_status: String)
 
     @PostMapping(path = ["/signup"])
     @ResponseBody
     fun signup(@RequestParam("username") username: String,
                @RequestParam("password") password: String,
-               @RequestParam("email") email: String): String {
+               @RequestParam("email") email: String): SignupResponse {
         val existingUsers : List<User> = repository.findByUsernameOrEmail(username, email) ?: listOf()
         if (existingUsers.isNotEmpty()) {
             val existingUser = existingUsers[0]
             if (existingUser.username == username)
-                return "username already exists."
+                return SignupResponse("username_already_exists")
             if (existingUser.email == email)
-                return "email already exists."
+                return SignupResponse("email_already_exists")
         }
 
         val hashedPassword = passwordEncoder.encode(password)
         val user = User(null, username, hashedPassword, Role.USER, email)
         return try {
             repository.save(user)
-            "sign up succeeded."
+            SignupResponse("signup_succeeded")
         } catch (ex: java.lang.RuntimeException) {
-            "sign up failed."
+            SignupResponse("signup_failed")
         }
     }
+
+    data class SignupResponse(val sign_status: String)
 }
